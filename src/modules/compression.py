e753a9c4231e79fb1bc5985c6b455d4cf97541d9
notunworthyalt <165967576+notunworthyalt@users.noreply.github.com>
2024-04-05 18:40:40 -0400

    Update compression.py

--- src/modules/compression.py
+++ src/modules/compression.py
@@ -49,7 +49,7 @@ class Compression:
                         num_prune = torch.floor(d * prune_ratio).long()
                         
                         total_neurons = d.size(0)
-                        desired_pruning_percentage = 0.95
+                        desired_pruning_percentage = 0.5
                         num_prune = torch.floor(d * desired_pruning_percentage).long()
                         
                         pivot_value = torch.sort(pivot_param.view(pivot_param.size(0), -1), dim=1)[0][
